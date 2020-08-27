0ac84e21bf42f7a0f2b9d5431bbb192fb6d02daf
jennysol <jennyssoliver@gmail.com>
2020-08-27 19:00:28 -0300

    Estilizando Dashboard

--- projeto/src/pages/Dashboard/index.tsx
+++ projeto/src/pages/Dashboard/index.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import{ Title } from   './styles';
+import logoImg from '../../asserts/img/logo.svg';
+
+import{ Title, Form, Repositories } from   './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositórios no Github</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/62623621?s=460&u=73119ca677ec564b7bdbf16acd27492acc010f2b&v=4"
+            alt="Jennifer Soliver"
+          />
+          <div>
+            <strong>jennysol/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native form!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/62623621?s=460&u=73119ca677ec564b7bdbf16acd27492acc010f2b&v=4"
+            alt="Jennifer Soliver"
+          />
+          <div>
+            <strong>jennysol/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native form!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/62623621?s=460&u=73119ca677ec564b7bdbf16acd27492acc010f2b&v=4"
+            alt="Jennifer Soliver"
+          />
+          <div>
+            <strong>jennysol/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native form!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/62623621?s=460&u=73119ca677ec564b7bdbf16acd27492acc010f2b&v=4"
+            alt="Jennifer Soliver"
+          />
+          <div>
+            <strong>jennysol/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native form!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- projeto/src/styles/global.ts
+++ projeto/src/styles/global.ts
@@ -11,7 +11,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: "#F0F0F5" url(${githubBackground}) no-repeat 70% top;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
   }
 
