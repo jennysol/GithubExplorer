c587e8ffc9db95c6ec98fee8859764a6d004f01b
jennysol <jennyssoliver@gmail.com>
2020-08-29 19:34:42 -0300

    Adicioanando navegaçao entre rotas

--- projeto/src/pages/Dashboard/index.tsx
+++ projeto/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState,useEffect, FormEvent} from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../asserts/img/logo.svg';
@@ -76,7 +77,7 @@ return (
 
       <Repositories>
         {repositories.map(repository => (
-            <a key={repository.full_name}  href="teste">
+            <Link key={repository.full_name}  to={`/repositories/${repository.full_name}`}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -87,7 +88,7 @@ return (
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- projeto/src/routes/index.tsx
+++ projeto/src/routes/index.tsx
@@ -7,7 +7,7 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch> // Garantir que apenas uma rota seja exibida
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
